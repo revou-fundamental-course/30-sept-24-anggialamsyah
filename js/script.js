67a41683ac13498bc66141ed88f45ad9a4f334bb
Anggi Alamsyah <anggialamsyah2@gmail.com>
2024-10-08 22:42:42 +0700

    create validation js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
+// Mengecek apakah Button dengan id = konversi di klik atau tidak
 if(document.getElementById("konversi")) {
-    // Javascript untuk menghitung Konversi Celcius ke Fahrenheit
+
+// Javascript untuk menghitung Konversi Celcius ke Fahrenheit
 // Event listener untuk Button konversi
 document.getElementById("konversi").addEventListener("click", function() {
 
-    // Fungsi untuk menghitung Konversi suhu dari Celcius ke Fahrenheit
-    function Konversi() {
-        let celcius = document.getElementById("input_suhu").value;
-    
-        fahrenheit = (celcius * 9/5) + 32;
+    // variabel input mengambil nilai dari id = input_suhu
+    let input = document.getElementById("input_suhu").value;
+    // Validasi inputan apakah angka atau bukan
+    if (!isNaN(input) && input.trim() !== "") {
+            // Fungsi untuk menghitung Konversi suhu dari Celcius ke Fahrenheit
+            function Konversi() {
+                let celcius = document.getElementById("input_suhu").value;
+                fahrenheit = (celcius * 9/5) + 32;
 
-        document.getElementById("hasil_suhu").value = fahrenheit + "\u00B0F";
+                document.getElementById("hasil_suhu").value = fahrenheit + "\u00B0F";
         
     }
 
-    // Memanggil Fungsi Konversi
-    Konversi();
+            // Memanggil Fungsi Konversi
+            Konversi();
 
-    // Fungsi untuk menjelaskan kalkulasi
-    function kalkulasi() {
-        let celcius = document.getElementById("input_suhu").value;
-        let hasil = document.getElementById("hasil_suhu").value;
+            // Fungsi untuk menjelaskan kalkulasi
+            function kalkulasi() {
+                let celcius = document.getElementById("input_suhu").value;
+                let hasil = document.getElementById("hasil_suhu").value;
 
-        cara = celcius + "\u00B0C "+ " *  (9/5) + "+ 32 + " = " + hasil;
-        document.getElementById("kalkulasi").value = cara;
+                cara = celcius + "\u00B0C "+ " *  (9/5) + "+ 32 + " = " + hasil;
+                document.getElementById("kalkulasi").value = cara;
     }
 
-    // Memanggil Fungsi Kalkulasi
-    kalkulasi();
-    
+                // Memanggil Fungsi Kalkulasi
+                kalkulasi();
+    } else {
+        alert("Inputan Tidak Valid : Inputan harus berupa angka dan tidak boleh kosong!");
+        document.getElementById("input_suhu").focus();
+        return;
+    } 
 });
 
 // Event Listener untuk Button Reset
@@ -37,34 +46,43 @@ document.getElementById("reset").addEventListener("click", function() {
 });
 } else {
 
-    // Javascript untuk menghitung Konversi Fahrenheit ke Celcius
+// Javascript untuk menghitung Konversi Fahrenheit ke Celcius
 // Event listener untuk Button konversi2
 document.getElementById("konversi2").addEventListener("click", function() {
 
-    // Fungsi untuk menghitung Konversi suhu dari Celcius ke Fahrenheit
-    function Konversi2() {
-        let fahrenheit = document.getElementById("input_suhu2").value;
-        celcius = 5/9 * (fahrenheit  - 32);
+    // variabel input mengambil nilai dari id = input_suhu2
+    let input = document.getElementById("input_suhu2").value;
+    // Validasi inputan apakah angka atau bukan
+    if (!isNaN(input) && input.trim() !== "") {
 
-        document.getElementById("hasil_suhu2").value = celcius + "\u00B0C";
+        // Fungsi untuk menghitung Konversi suhu dari Celcius ke Fahrenheit
+        function Konversi2() {
+            let fahrenheit = document.getElementById("input_suhu2").value;
+            celcius = 5/9 * (fahrenheit  - 32);
+
+             document.getElementById("hasil_suhu2").value = celcius + "\u00B0C";
         
     }
 
-    // Memanggil Fungsi Konversi2
-    Konversi2();
+        // Memanggil Fungsi Konversi2
+        Konversi2();
 
-    // Fungsi untuk menjelaskan kalkulasi
-    function kalkulasi2() {
-        let fahrenheit = document.getElementById("input_suhu2").value;
-        let hasil = document.getElementById("hasil_suhu2").value;
+        // Fungsi untuk menjelaskan kalkulasi
+        function kalkulasi2() {
+            let fahrenheit = document.getElementById("input_suhu2").value;
+            let hasil = document.getElementById("hasil_suhu2").value;
 
-        cara = " 5/9 * ( " + fahrenheit + "\u00B0F ) - "+ 32 + " = " + hasil;
-        document.getElementById("kalkulasi2").value = cara;
+            cara = " 5/9 * ( " + fahrenheit + "\u00B0F ) - "+ 32 + " = " + hasil;
+            document.getElementById("kalkulasi2").value = cara;
     }
 
-    // Memanggil Fungsi Kalkulasi2
-    kalkulasi2();
-    
+            // Memanggil Fungsi Kalkulasi2
+            kalkulasi2();
+    } else {
+        alert("Inputan Tidak Valid : Inputan harus berupa angka dan tidak boleh kosong!");
+        document.getElementById("input_suhu2").focus();
+        return;
+    }
 });
 
 // Event Listener untuk Button Reset
